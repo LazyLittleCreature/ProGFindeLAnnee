965c84f0c33baeb25fac6d99122cf02ce337220b
LazyLittleCreature <lazylittlecreature@outlook.com>
2024-06-04 19:57:16 +0200

    score functional
    it works
    fuc*ing score

--- Assets/Scripts/other/game management/end of level.cs
+++ Assets/Scripts/other/game management/end of level.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class endOfLevel : MonoBehaviour
 { 
@@ -10,7 +12,11 @@ public class endOfLevel : MonoBehaviour
     private bool bigPlayerWin = false;
     private bool smallPlayerWin = false;
     [SerializeField] private Canvas canvas;
-
+    // ----
+    public TextMeshProUGUI scoreText;
+    private int timeInSecs;
+    // Score
+    
     private void Start()
     {
         canvas.gameObject.SetActive(false);
@@ -18,6 +24,20 @@ public class endOfLevel : MonoBehaviour
 
     void Update()
     {
+        // ----
+        if (GameOn)
+        {
+            timeInSecs = Mathf.FloorToInt(Time.timeSinceLevelLoad);
+            var mins = timeInSecs / 60;
+            var secs = timeInSecs % 60;
+        }
+        else
+        {
+            //scoreText.text = playerScore.ToString("F2");
+            scoreText.text = $"{timeInSecs / 60} mins {timeInSecs % 60} secs";
+        }
+        // Score
+        
         if (bigPlayerWin == true && smallPlayerWin == true)
         {
             GameOn = false;
