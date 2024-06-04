50c06a00535e0070b28d9de0332edafda06ca788
LazyLittleCreature <lazylittlecreature@outlook.com>
2024-06-04 22:53:04 +0200

    Volumebar finally normal
    it doesn't work yet because no script but it's there

---
+++ Assets/Scripts/UI/VolumeBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeBar : MonoBehaviour
+{
+    public void OnSliderChanged(float value)
+    {
+        
+    }
+}
